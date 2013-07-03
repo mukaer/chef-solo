7fa728ae8e18ff9daa72f5a9c540f7e9e31bd98e
muaker <mukaer@gmail.com>
2013-07-04 03:52:12 +0900

    add ruby

---
+++ cookbooks/ruby/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: ruby
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+RUBY_VER= node[:ruby][:version]
+
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+include_recipe "rbenv::rbenv_vars"
+
+rbenv_ruby RUBY_VER
+
+rbenv_gem "bundler" do
+  ruby_version RUBY_VER
+end  
+
+execute "rbenv global" do
+  command "source /etc/profile  && rbenv global  #{RUBY_VER}"  
+end
