8d56c985a8d87aeb5c03a58c14d218dd55992532
muaker <mukaer@gmail.com>
2013-07-15 18:17:40 +0900

    fix publify

--- cookbooks/publify/recipes/db_create.rb
+++ cookbooks/publify/recipes/db_create.rb
@@ -1,7 +1,7 @@
 cmd  = "su - #{node[:publify][:user]} -c '"
 cmd += "source /etc/profile && cd #{node[:publify][:app_root]} "
 cmd += "&& rake db:create "
-cmd += " RAILS_ENV=#{node[publify][:rails_env]}" unless node[publify][:rails_env].empty?
+cmd += " RAILS_ENV=#{node[:publify][:rails_env]}" unless node[:publify][:rails_env].empty?
 cmd += "'"
 
 execute "install command" do
